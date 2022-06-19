b6e129867cac610faf86ccffd6791aeb7d70bf0a
tatsuki-yamamoto <yama.tatsuki.0204@gmail.com>
2022-06-19 16:42:13 +0900

    feat: 基本的な内容の修正

--- components/MyComponent.vue
+++ components/MyComponent.vue
@@ -1,19 +1,7 @@
 <script lang="ts" setup>
 import type { Ref, ComputedRef } from 'vue'
 
-// props
-interface Props {
-  name: string
-}
-const props = defineProps<Props>()
-
-// emit
-interface Emits {
-  (event: 'change', value: string): void
-}
-const emit = defineEmits<Emits>()
-
-// data
+// ref
 // ref関数を設定することでReactiveな変数を定義できる。ref関数の引数には初期値を設定できる。
 // 型推論で型指定を省略できる
 const msg = ref('Hello!')
@@ -22,12 +10,12 @@ const msg2 = ref<string>('Hello!')
 // ComputedRefで明示的に型指定できる
 const msg3: Ref<string> = ref('Hello!')
 
+// reactive
 const user = reactive({
   firstName: 'John',
   lastName: 'Doe',
   age: 25,
 })
-
 interface User2 {
   firstName: string
   lastName: string
@@ -38,100 +26,78 @@ const user2: User2 = reactive({
   lastName: 'Doe',
   age: 25,
 })
+const changeName = (event: Event) => {
+  console.log('changeName')
+  user.firstName = (event.target as HTMLInputElement).value
+}
+// 変数の方にも型定義できるけど、これはやりすぎかな
+const changeName2: (event: Event) => void = (event: Event): void => {
+  user.firstName = (event.target as HTMLInputElement).value
+}
 
-// computed
-const count = ref(0)
+const counter = ref(0)
+const increment = () => {
+  console.log('increment!')
+  counter.value++
+}
+const decrement = () => {
+  console.log('decrement!')
+  counter.value--
+}
+// 旧computedに相当
 // 型推論で型指定を省略できる
-const countValue = computed(() => `カウント＝${count.value}`)
+const counterLabel = computed(() => {
+  console.log('computed!', { 'counter.value': counter.value })
+  return `カウンターは「${counter.value}」です！`
+})
 // ジェネリクスで明示的に型指定できる
-const countValue2 = computed<string>(() => `カウント＝${count.value}`)
+const counterValue2 = computed<string>(() => `カウンターは「${counter.value}」です！`)
 // ComputedRefで明示的に型指定できる
-const countValue3: ComputedRef<string> = computed(() => `カウント＝${count.value}`)
+const counterValue3: ComputedRef<string> = computed(() => `カウンターは「${counter.value}」です！`)
 
-// watch
-// inputName の監視
-const price = ref(1000)
-const valueCount = ref(0)
-const sumPrice = ref(0)
-watch(price, (newValue, oldValue) => {
-  console.log({ newValue, oldValue })
-})
-watchEffect(() => {
-  sumPrice.value = valueCount.value * price.value
-  console.log({ price: price.value, count: valueCount.value, sumPrice: sumPrice.value })
-})
-
-const inputName = ref<string>('Johnだろーが')
-const input = ref<HTMLInputElement>()
-// mounted
+// 旧mountedに相当
 onMounted(() => {
   console.log('mounted!')
-  console.log({ 'input.value?.value': input.value?.value })
-})
-// updated
-onUpdated(() => {
-  console.log('updated!')
 })
 
-// destroyed
+// 旧updatedに相当
+onUpdated(() => {
+  console.log('updated!')
+  console.log({ msg })
+})
+
+// 旧destroyedに相当
 onUnmounted(() => {
   console.log('unmounted!')
 })
-
-// methods
-const countUp = () => {
-  count.value++
-}
-const changeName = (newName: string) => {
-  user.firstName = newName
-}
-const changeName2 = (newName: string): void => {
-  user.firstName = newName
-}
-// 変数の方にも型定義できるけど、これはやりすぎかな
-const changeName3: (newName: string) => void = (newName: string): void => {
-  user.firstName = newName
-}
-const change = (): void => {
-  emit('change', 'Jane')
-}
 </script>
 
 <template>
   <div>
-    <div class="mb-8">
-      <p class="text-xl font-bold">リアクティブな変数の表示</p>
-      <p>{{ msg }}</p>
-    </div>
+    <base-sub-title-and-contants title="ref">
+      <p class="mb-2">{{ msg }}</p>
+      <v-row no-gutters>
+        <v-col cols="12" lg="6">
+          <v-text-field v-model="msg" variant="outlined" hide-details></v-text-field>
+        </v-col>
+      </v-row>
+    </base-sub-title-and-contants>
 
-    <div class="mb-8">
-      <p class="text-xl font-bold">リアクティブな変数の表示</p>
-      <p>{{ countValue }}</p>
-      <v-btn color="primary" @click="countUp()">カウントアップ</v-btn>
-    </div>
+    <base-sub-title-and-contants title="reactive">
+      <p class="mb-2">フルネーム: {{ `${user.firstName} ${user.lastName}（${user.age}）` }}</p>
+      <v-text-field :model-value="user.firstName" variant="outlined" hide-details @input="changeName"></v-text-field>
+    </base-sub-title-and-contants>
 
-    <div class="mb-8">
-      <p class="text-xl font-bold">methodsの確認</p>
-      <p>FullName: {{ `${user.firstName} ${user.lastName}（${user.age}）` }}</p>
-      <v-btn color="primary" @click="changeName('ほげ')">名前変更</v-btn>
-    </div>
+    <base-sub-title-and-contants title="computed">
+      <p class="mb-2">{{ counterLabel }}</p>
+      <v-btn-group color="primary">
+        <v-btn @click="increment"><v-icon>mdi-plus</v-icon></v-btn>
+        <v-btn @click="decrement"><v-icon>mdi-minus</v-icon></v-btn>
+      </v-btn-group>
+    </base-sub-title-and-contants>
 
-    <div class="mb-8">
-      <p class="text-xl font-bold">propsとemitsの確認</p>
-      <p>propsのnameは"{{ name }}"です</p>
-      <v-btn color="primary" @click="change">emits</v-btn>
-    </div>
-
-    <div class="mb-8">
-      <p class="text-xl font-bold">マウント後にinputのvalueが取得できるか確認</p>
-      <v-text-field ref="input" v-model="inputName"></v-text-field>
-    </div>
-
-    <div class="mb-8">
-      <p class="text-xl font-bold">wacthの確認</p>
-      <div>値段：<input v-model="price" />円</div>
-      <div>個数：<input v-model="valueCount" />個</div>
-      <div>合計：{{ sumPrice }}円</div>
-    </div>
+    <base-sub-title-and-contants title="onMounted・onUpdated・onUnmounted">
+      <p>ログに出力</p>
+    </base-sub-title-and-contants>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,6 @@
-<script lang="ts" setup>
-const name = ref('サンプルコンポーネント')
-const changeName = (newName: string): void => {
-  name.value = newName
-}
-</script>
-
 <template>
   <base-container>
-    <base-title hide-back-button>TOPページ</base-title>
-    <my-component :name="name" @change="changeName"></my-component>
+    <base-title hide-back-button>基本的な内容</base-title>
+    <my-component></my-component>
   </base-container>
 </template>
