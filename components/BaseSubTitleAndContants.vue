24217309e970955cb2a8ca4f5915dc8625440f8b
tatsuki-yamamoto <yama.tatsuki.0204@gmail.com>
2022-06-19 13:37:56 +0900

    feat: サブヘッダーコンポーネント新規作成

---
+++ components/BaseSubTitleAndContants.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+interface Props {
+  title: string
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="mt-3 mb-9">
+    <base-sub-title>{{ title }}</base-sub-title>
+    <slot />
+  </div>
+</template>
