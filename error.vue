ea6e7c846ebf331532c75ad442ef15804aca176b
tatsuki-yamamoto <yama.tatsuki.0204@gmail.com>
2022-06-19 19:11:55 +0900

    feat: カスタムエラーページ 新規追加

---
+++ error.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+useHead({
+  title: 'ページが表示できませんでした',
+})
+const error = useError()
+</script>
+
+<template>
+  <base-container class="text-center">
+    <h1 class="text-3xl font-bold inline">ページが表示できませんでした</h1>
+    <p class="mt-6">時間をおいて再訪問をお願いします。</p>
+    <v-card variant="outlined" class="mt-9 mb-12">
+      <v-container>
+        <v-row class="mb-6">
+          <v-col cols="3" class="text-right font-bold">ステータスコード</v-col>
+          <v-col cols="6" class="text-left">{{ error.statusCode }}</v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="3" class="text-right font-bold">メッセージ</v-col>
+          <v-col cols="6" class="text-left">{{ error.message }}</v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+    <v-btn @click="$router.back()">前のページへ戻る</v-btn>
+  </base-container>
+</template>
---
+++ pages/404.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useHead({
+  title: 'お探しのページが見つかりませんでした',
+})
+const error = useError()
+</script>
+
+<template>
+  <base-container class="text-center">
+    <h1 class="text-3xl font-bold inline">お探しのページが見つかりませんでした</h1>
+    <p class="mt-6 mb-9">あなたがアクセスしたページは、削除されたかURLが変更されているため<br />表示することができませんでした。</p>
+    <v-btn nuxt to="/">TOPページへ戻る</v-btn>
+  </base-container>
+</template>
