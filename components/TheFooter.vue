d12386b7c776c86529a5dadb481ecb6cee87c796
tatsuki-yamamoto <yama.tatsuki.0204@gmail.com>
2022-06-19 13:33:33 +0900

    feat: フッター 右寄せに修正

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <v-footer class="p-0">
-    <v-card elevation="0" rounded="0" class="text-center w-full">
+    <v-card elevation="0" rounded="0" class="text-right w-full">
       <v-card-text>
         <a href="https://twitter.com/tatsuki_work" target="_blank" rel="noopener noreferrer" class="no-underline"> @tatsuki_work </a>
         {{ ' ' + new Date().getFullYear() }}. Some rights reserved.
